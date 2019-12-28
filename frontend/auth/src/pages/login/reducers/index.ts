bde410ee872dd403ee2cd865024a05c392b9cbf4
Front-start <neverendingfront@gmail.com>
2019-12-28 05:30:32 +0300

    fix(auth): fix reg/login input update

--- frontend/auth/src/pages/login/reducers/index.ts
+++ frontend/auth/src/pages/login/reducers/index.ts
@@ -1,14 +1,25 @@
 import { createReducer } from '@utils/reducer'
 import * as actions from '../constants'
 
-const initialState = {
+interface Errors {
+  email?: string
+  password?: string
+}
+
+interface State {
+  email: string
+  password: string
+  errors: Errors
+}
+
+const initialState: State = {
   email: '',
   password: '',
   errors: {},
 }
 
 export default createReducer(initialState, {
-  [actions.change]: (state, { field, value }) => ({ ...state, field: value }),
+  [actions.change]: (state, { field, value }) => ({ ...state, [field]: value }),
   [actions.setErrors]: (state, { errors }) => ({ ...state, errors }),
   [actions.clear]: () => initialState,
 })
